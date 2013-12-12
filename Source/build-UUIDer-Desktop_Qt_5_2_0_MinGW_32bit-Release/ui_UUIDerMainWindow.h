dd97fb3950d941dfe34e62e71f0945cde632880f
Ben <y.x.ben.h+github@gmail.com>
2013-12-12 13:15:22 +1100

    Windows build

--- Source/build-UUIDer-Desktop_Qt_5_2_0_MinGW_32bit-Release/ui_UUIDerMainWindow.h
+++ Source/build-UUIDer-Desktop_Qt_5_2_0_MinGW_32bit-Release/ui_UUIDerMainWindow.h
@@ -13,15 +13,18 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QSpinBox>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -32,14 +35,22 @@ public:
     QAction *action;
     QAction *actionAbout;
     QWidget *centralWidget;
+    QVBoxLayout *verticalLayout_4;
+    QVBoxLayout *verticalLayout_3;
+    QVBoxLayout *verticalLayout_2;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *pushButton;
+    QSpinBox *spinBox;
     QLabel *label;
+    QHBoxLayout *horizontalLayout_2;
+    QPushButton *pushButton_2;
+    QSpinBox *spinBox_2;
     QLabel *label_2;
+    QHBoxLayout *horizontalLayout_3;
     QLabel *label_3;
     QLabel *label_4;
-    QPushButton *pushButton;
-    QPushButton *pushButton_2;
-    QSpinBox *spinBox;
-    QSpinBox *spinBox_2;
+    QSpacerItem *horizontalSpacer;
+    QSpacerItem *verticalSpacer;
     QMenuBar *menuBar;
     QMenu *menuFile;
     QMenu *menuHelp;
@@ -50,41 +61,126 @@ public:
     {
         if (UUIDerMainWindow->objectName().isEmpty())
             UUIDerMainWindow->setObjectName(QStringLiteral("UUIDerMainWindow"));
-        UUIDerMainWindow->resize(400, 300);
+        UUIDerMainWindow->resize(422, 265);
         action = new QAction(UUIDerMainWindow);
         action->setObjectName(QStringLiteral("action"));
         actionAbout = new QAction(UUIDerMainWindow);
         actionAbout->setObjectName(QStringLiteral("actionAbout"));
         centralWidget = new QWidget(UUIDerMainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        label = new QLabel(centralWidget);
-        label->setObjectName(QStringLiteral("label"));
-        label->setGeometry(QRect(260, 20, 46, 13));
-        label_2 = new QLabel(centralWidget);
-        label_2->setObjectName(QStringLiteral("label_2"));
-        label_2->setGeometry(QRect(260, 60, 46, 13));
-        label_3 = new QLabel(centralWidget);
-        label_3->setObjectName(QStringLiteral("label_3"));
-        label_3->setGeometry(QRect(50, 200, 46, 13));
-        label_4 = new QLabel(centralWidget);
-        label_4->setObjectName(QStringLiteral("label_4"));
-        label_4->setGeometry(QRect(150, 200, 46, 13));
+        verticalLayout_4 = new QVBoxLayout(centralWidget);
+        verticalLayout_4->setSpacing(6);
+        verticalLayout_4->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_4->setObjectName(QStringLiteral("verticalLayout_4"));
+        verticalLayout_3 = new QVBoxLayout();
+        verticalLayout_3->setSpacing(6);
+        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setSpacing(6);
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setSizeConstraint(QLayout::SetDefaultConstraint);
         pushButton = new QPushButton(centralWidget);
         pushButton->setObjectName(QStringLiteral("pushButton"));
-        pushButton->setGeometry(QRect(30, 20, 75, 23));
-        pushButton_2 = new QPushButton(centralWidget);
-        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
-        pushButton_2->setGeometry(QRect(30, 60, 75, 23));
+        QSizePolicy sizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(pushButton->sizePolicy().hasHeightForWidth());
+        pushButton->setSizePolicy(sizePolicy);
+
+        horizontalLayout->addWidget(pushButton);
+
         spinBox = new QSpinBox(centralWidget);
         spinBox->setObjectName(QStringLiteral("spinBox"));
-        spinBox->setGeometry(QRect(170, 20, 71, 22));
+        QSizePolicy sizePolicy1(QSizePolicy::Expanding, QSizePolicy::Fixed);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(spinBox->sizePolicy().hasHeightForWidth());
+        spinBox->setSizePolicy(sizePolicy1);
+        spinBox->setMinimumSize(QSize(0, 0));
+        spinBox->setBaseSize(QSize(0, 0));
+
+        horizontalLayout->addWidget(spinBox);
+
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        QSizePolicy sizePolicy2(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy2.setHorizontalStretch(0);
+        sizePolicy2.setVerticalStretch(0);
+        sizePolicy2.setHeightForWidth(label->sizePolicy().hasHeightForWidth());
+        label->setSizePolicy(sizePolicy2);
+
+        horizontalLayout->addWidget(label);
+
+
+        verticalLayout_2->addLayout(horizontalLayout);
+
+
+        verticalLayout_3->addLayout(verticalLayout_2);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setSpacing(6);
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        pushButton_2 = new QPushButton(centralWidget);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        sizePolicy.setHeightForWidth(pushButton_2->sizePolicy().hasHeightForWidth());
+        pushButton_2->setSizePolicy(sizePolicy);
+
+        horizontalLayout_2->addWidget(pushButton_2);
+
         spinBox_2 = new QSpinBox(centralWidget);
         spinBox_2->setObjectName(QStringLiteral("spinBox_2"));
-        spinBox_2->setGeometry(QRect(170, 60, 71, 22));
+        sizePolicy1.setHeightForWidth(spinBox_2->sizePolicy().hasHeightForWidth());
+        spinBox_2->setSizePolicy(sizePolicy1);
+
+        horizontalLayout_2->addWidget(spinBox_2);
+
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        QSizePolicy sizePolicy3(QSizePolicy::Preferred, QSizePolicy::Fixed);
+        sizePolicy3.setHorizontalStretch(0);
+        sizePolicy3.setVerticalStretch(0);
+        sizePolicy3.setHeightForWidth(label_2->sizePolicy().hasHeightForWidth());
+        label_2->setSizePolicy(sizePolicy3);
+
+        horizontalLayout_2->addWidget(label_2);
+
+
+        verticalLayout_3->addLayout(horizontalLayout_2);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setSpacing(6);
+        horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
+        label_3 = new QLabel(centralWidget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+
+        horizontalLayout_3->addWidget(label_3);
+
+        label_4 = new QLabel(centralWidget);
+        label_4->setObjectName(QStringLiteral("label_4"));
+
+        horizontalLayout_3->addWidget(label_4);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_3->addItem(horizontalSpacer);
+
+
+        verticalLayout_3->addLayout(horizontalLayout_3);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_3->addItem(verticalSpacer);
+
+
+        verticalLayout_4->addLayout(verticalLayout_3);
+
         UUIDerMainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(UUIDerMainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 400, 21));
+        menuBar->setGeometry(QRect(0, 0, 422, 21));
         menuFile = new QMenu(menuBar);
         menuFile->setObjectName(QStringLiteral("menuFile"));
         menuHelp = new QMenu(menuBar);
@@ -113,12 +209,12 @@ public:
         UUIDerMainWindow->setWindowTitle(QApplication::translate("UUIDerMainWindow", "UUIDer", 0));
         action->setText(QApplication::translate("UUIDerMainWindow", "???", 0));
         actionAbout->setText(QApplication::translate("UUIDerMainWindow", "About", 0));
+        pushButton->setText(QApplication::translate("UUIDerMainWindow", "Generate", 0));
         label->setText(QApplication::translate("UUIDerMainWindow", "UUID's", 0));
+        pushButton_2->setText(QApplication::translate("UUIDerMainWindow", "Get", 0));
         label_2->setText(QApplication::translate("UUIDerMainWindow", "UUID's", 0));
         label_3->setText(QApplication::translate("UUIDerMainWindow", "TextLabel", 0));
         label_4->setText(QApplication::translate("UUIDerMainWindow", "TextLabel", 0));
-        pushButton->setText(QApplication::translate("UUIDerMainWindow", "Generate", 0));
-        pushButton_2->setText(QApplication::translate("UUIDerMainWindow", "Get", 0));
         menuFile->setTitle(QApplication::translate("UUIDerMainWindow", "File", 0));
         menuHelp->setTitle(QApplication::translate("UUIDerMainWindow", "Help", 0));
     } // retranslateUi
