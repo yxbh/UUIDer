d161694ff18271b7425228443d8bf547e11bd0a6
Ben <y.x.ben.h+github@gmail.com>
2013-12-17 20:26:03 +1100

    improvements to RandomStringGenerator

--- Source/UUIDer/RandomStringGenerator.cpp
+++ Source/UUIDer/RandomStringGenerator.cpp
@@ -1,31 +1,41 @@
 #include "RandomStringGenerator.hpp"
-#include <random>
+#include <chrono>
+#include <QDebug>
 
+static QString PossibleCharacters
+    ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=_+!@#$\%^&*()`~<>?:\'\";,.\\| ");
 
 namespace BH
 {
 
-RandomStringGenerator::StringType RandomStringGenerator::s_PossibleChars("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=_+!@#$\%^&*()`~");
+using namespace std::chrono;
+
+RandomStringGenerator::GeneratorType RandomStringGenerator::s_Generator
+    (high_resolution_clock::now().time_since_epoch().count());
 
 RandomStringGenerator::StringType RandomStringGenerator::Generate(const unsigned p_StrLength)
 {
-    std::default_random_engine rand_eng;
     unsigned random_str_len;
     if (p_StrLength != 0)
         random_str_len = p_StrLength;
     else
     {
         std::uniform_int_distribution<unsigned> dist;
-        random_str_len = dist(rand_eng);
+        random_str_len = dist(RandomStringGenerator::GetGenerator());
     }
 
     QString random_str;
     random_str.reserve(random_str_len + 1);
-    std::uniform_int_distribution<unsigned> char_distribution(0, s_PossibleChars.length()-1);
+    std::uniform_int_distribution<unsigned> char_distribution(0, PossibleCharacters.length()-1);
     for (unsigned i{0}; i != random_str_len; ++i)
-        random_str += s_PossibleChars.at(char_distribution(rand_eng));
+        random_str += PossibleCharacters.at(char_distribution(RandomStringGenerator::GetGenerator()));
 
     return random_str;
+}
+
+RandomStringGenerator::GeneratorType & RandomStringGenerator::GetGenerator(void)
+{
+    return s_Generator;
 }
 
 } // BH ns
--- Source/UUIDer/RandomStringGenerator.hpp
+++ Source/UUIDer/RandomStringGenerator.hpp
@@ -1,6 +1,6 @@
 #pragma once
+#include <random>
 #include <QString>
-
 
 namespace BH
 {
@@ -9,15 +9,19 @@ namespace BH
 class RandomStringGenerator
 {
 public:
-	using StringType = QString;
+    using StringType = QString;
+    using GeneratorType = std::default_random_engine;
 
 private:
-	static StringType s_PossibleChars;
+    static GeneratorType s_Generator;
 
 public:
 	RandomStringGenerator(void) = delete;
 
-    static StringType Generate(const unsigned p_StrLength = 0);
+    static StringType Generate(const unsigned p_StrLength = 200);
+
+private:
+    static GeneratorType & GetGenerator(void);
 
 }; // RandomStringGenerator class
 
