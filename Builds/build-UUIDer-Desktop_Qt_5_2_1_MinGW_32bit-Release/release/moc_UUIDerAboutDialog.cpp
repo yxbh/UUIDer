c440bdb2cbada9235f547f37dd1772d7cbdab6a7
Ben <y.x.ben.h+github@gmail.com>
2014-02-26 18:03:20 +0800

    fixed new build build location.

--- Builds/build-UUIDer-Desktop_Qt_5_2_1_MinGW_32bit-Release/release/moc_UUIDerAboutDialog.cpp
+++ Builds/build-UUIDer-Desktop_Qt_5_2_1_MinGW_32bit-Release/release/moc_UUIDerAboutDialog.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../UUIDer/UUIDerAboutDialog.hpp"
+#include "../../../Source/UUIDer/UUIDerAboutDialog.hpp"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
