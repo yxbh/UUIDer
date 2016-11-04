3bd2817495a6e30d4f4f5a5fd71ea27082713379
Benjamin Huang <y.x.ben.h+github@gmail.com>
2016-11-04 20:18:00 +1100

    appveyor fix

--- Scripts/CI/AppVeyor/build_script.ps1
+++ Scripts/CI/AppVeyor/build_script.ps1
@@ -4,14 +4,14 @@ cd Source
 
 # build DEBUG build.
 qmake UUIDer.pro "CONFIG+=debug"
-IF ($LASTEXITCODE -ne 0){exit $LASTEXITCODE}
+IF ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 mingw32-make
-IF ($LASTEXITCODE -ne 0){exit $LASTEXITCODE}
+IF ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 # build RELEASE build.
 qmake UUIDer.pro
-IF ($LASTEXITCODE -ne 0){exit $LASTEXITCODE}
+IF ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 mingw32-make
-IF ($LASTEXITCODE -ne 0){exit $LASTEXITCODE}
+IF ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
